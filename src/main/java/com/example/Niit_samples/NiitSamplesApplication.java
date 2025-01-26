be3c5d87399f0e4f8248b4942d7710dfa32f8876
AliyuAliyu1 <109520063+AliyuAliyu1@users.noreply.github.com>
2025-01-26 17:35:08 +0100

    first commit

---
+++ src/main/java/com/example/Niit_samples/NiitSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Niit_samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiitSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiitSamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Niit_samples/NiitSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Niit_samples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiitSamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
